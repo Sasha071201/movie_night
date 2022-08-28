3f317f643d741f1a195421d07b1b39f244c30a2c
Sasha071201 <37871754+Sasha071201@users.noreply.github.com>
2022-08-28 17:01:19 +0300

    Fixed iOS build (#12)

--- lib/application/domain/ad_mob/app_lifecycle_reactor.dart
+++ lib/application/domain/ad_mob/app_lifecycle_reactor.dart
@@ -1,4 +1,4 @@
-import 'package:google_mobile_ads/google_mobile_ads.dart';
+// import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:movie_night/application/domain/ad_mob/ad_helper.dart';
 
 /// Listens for app foreground events and shows app open ads.
@@ -8,16 +8,16 @@ class AppLifecycleReactor {
   AppLifecycleReactor({required this.adHelper});
 
   void listenToAppStateChanges() {
-    AppStateEventNotifier.startListening();
-    AppStateEventNotifier.appStateStream
-        .forEach((state) => _onAppStateChanged(state));
+    // AppStateEventNotifier.startListening();
+    // AppStateEventNotifier.appStateStream
+    // .forEach((state) => _onAppStateChanged(state));
   }
 
-  void _onAppStateChanged(AppState appState) {
-    // Try to show an app open ad if the app is being resumed and
-    // we're not already showing an app open ad.
-    if (appState == AppState.foreground) {
-      adHelper.showAdIfAvailable();
-    }
-  }
+  // void _onAppStateChanged(AppState appState) {
+  //   // Try to show an app open ad if the app is being resumed and
+  //   // we're not already showing an app open ad.
+  //   if (appState == AppState.foreground) {
+  //     adHelper.showAdIfAvailable();
+  //   }
+  // }
 }
